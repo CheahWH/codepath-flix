07cedcac8d48ada8c4fd99940a8649246b4964ba
CheahWH <78494351+CheahWH@users.noreply.github.com>
2022-03-04 01:42:39 -0800

    right before collection view layout

---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Cheah Wen Hui on 03/03/2022.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
